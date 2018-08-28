efabe9453c389f33343bb89681199355e3736855
codiechanel <codiechanel@gmail.com>
2018-08-29 05:26:08 +0800

    init

--- src/SideBar.tsx
+++ src/SideBar.tsx
@@ -11,10 +11,23 @@ class SideBar extends React.Component<any, any> {
     let arr = this.props.store.repos.entries();
     let list: any = Array.from(arr);
 
+      /**
+       *   the innerNavContent's purpose is to organize the flow * of this
+       section with the list...  it allows the list to be pulled upwards
+       and to set min-width zero for the firefox scroll fix
+       */
+
     return (
+        /**
+         * the function of navContent is to enforce the height limit for it's
+         * direct child...and to occupy the whole space from parent nav
+         */
       <div className={"navContent"}>
+
+
         <div className={"innerNavContent"}>
-          <div style={{  }}>
+
+          <div style={{}}>
             <h3>We like balloons</h3>
             <p>
               You can find all kinds of balloon related things here and there

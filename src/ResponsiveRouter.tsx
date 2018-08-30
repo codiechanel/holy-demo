18473fb9475d3d00837fbea9df6d7a0e9e4d4b09
codiechanel <codiechanel@gmail.com>
2018-08-30 13:06:38 +0800

    init

--- src/ResponsiveRouter.tsx
+++ src/ResponsiveRouter.tsx
@@ -11,42 +11,21 @@ class ResponsiveRouter extends React.Component<any, any> {
   };
 
   render() {
-      console.log('ResponsiveRouter',  this.props.width )
+    console.log("ResponsiveRouter", this.props.width);
     if (isWidthDown("xs", this.props.width)) {
       // return default first child immediately
       return this.props.children[0];
     }
 
-    // let routes = this.props.store.selectedRoutes;
-    // let routeSource = routes[routes.length - 1];
-    // console.log("routeSource", routeSource);
     let list = [];
     let children = this.props.children;
     let ctr = 0;
     let stop = false;
 
     React.Children.forEach(children, (child: any) => {
-      // console.log("isWidthDown ctr", ctr);
-
       if (stop) {
         // noop
       } else {
-        // if (isWidthDown("xs", this.props.width) && ctr == 0) {
-        //   // just get first child then ignore the rest
-        //   // push first child always
-        //
-        //     // if (routeSource) {
-        //   //   list.push(this.matchRoute(children, routeSource));
-        //   // } else {
-        //   //   list.push(child);
-        //   // }
-        //   /**
-        //    * since we are in a small screen
-        //    * we only to return one child
-        //    */
-        //   list.push(child);
-        //   stop = true;
-        // } else
         if (isWidthDown("sm", this.props.width)) {
           // let's push 2 children and ignore the 3rd
           if (ctr < 2) {
@@ -61,7 +40,6 @@ class ResponsiveRouter extends React.Component<any, any> {
       }
 
       ctr++;
-      // list.push(child)
     });
     return list.map(x => x);
   }
